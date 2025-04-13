57992dfe6b977328f37626c0b87b94bb64ab218e
mssreehari <sreeharimanezhath@gmail.com>
2025-04-13 21:39:41 +0530

    commit

--- canfinity/lib/config/api_keys.dart
+++ canfinity/lib/config/api_keys.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiKeys {
-  static const huggingFaceApiKey = 'PASTE_YOUR_API_KEY'; // Replace with your token
+  static const huggingFaceApiKey = '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'; // Replace with your token
 } 
